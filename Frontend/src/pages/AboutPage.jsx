7bbdcc9be168add92943d4ee062dd929c4e6ed61
Alex <adicicco@oakland.edu>
2025-07-23 13:24:47 -0400

    Add AboutPage with team and project description

---
+++ Frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>About PhishFinder</h1>
+      <p>PhishFinder is a machine learning tool that helps detect phishing emails.</p>
+      <p>Developed by Alexander DiCicco, Derek Schramm, Matthew Kajdasz, Lucas Hermiz.</p>
+      <a href="/">Back to Home</a>
+    </div>
+  );
+}
+
+export default AboutPage;
